c5491c0add20aaa104c6121b75d540276c3581a2
Syed Paymaan Raza <paymaan.syed@gmail.com>
2017-02-12 00:05:25 -0500

    basic input checking and initial configure functionality

--- aws-ec2-automate.sh
+++ aws-ec2-automate.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+commandType=$1
+artifactsPath=~/aws-automate-ec2-artifacts
+
+# command input checking
+if ! [[ "$commandType" = "configure"          \
+              || "$commandType" = "launch"    \
+              || "$commandType" = "terminate" \
+              || "$commandType" = "status" ]]; then
+    echo "usage: ./aws-ec2-automate.sh configure|launch|terminate|status"
+    exit
+fi
+
+# configure
+configFilePath=${artifactsPath}/config.cfg
+if [[ "$commandType" = "configure" ]]; then
+    if ! [[ -f "$configFilePath" ]]; then
+        mkdir -p $artifactsPath && touch $configFilePath
+    fi
+fi
+
+exit 0
